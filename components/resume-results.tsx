3c232597b426a01a27ba9c95355c996edb23d827
Shashivarunreddy <137184492+Shashivarunreddy@users.noreply.github.com>
2024-11-23 01:08:40 +0530

    updated use-toste file

--- components/resume-results.tsx
+++ components/resume-results.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from 'react'
+
 import { useOptimization } from '@/context/OptimizationContext'
 import { Button } from "@/components/ui/button"
 import { useToast } from "@/components/ui/use-toast"
